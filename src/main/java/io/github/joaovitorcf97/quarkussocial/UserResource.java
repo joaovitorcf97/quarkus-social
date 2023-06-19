1a85715dff1b55eeb5d0822e97aeac10764ca7cb
João Vitor <joaovitorcfdev@gmail.com>
2023-06-18 21:50:22 -0300

    create user crud

--- src/main/java/io/github/joaovitorcf97/quarkussocial/UserResource.java
+++ src/main/java/io/github/joaovitorcf97/quarkussocial/UserResource.java
@@ -1,10 +1,17 @@
 package io.github.joaovitorcf97.quarkussocial;
 
+import io.github.joaovitorcf97.quarkussocial.domain.model.User;
 import io.github.joaovitorcf97.quarkussocial.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -15,13 +22,53 @@ import jakarta.ws.rs.core.Response;
 public class UserResource {
 
     @POST
+    @Transactional
     public Response createUser(CreateUserRequest userRequest) {
-        System.out.println(userRequest.getName());
-        return Response.ok().build();
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
+        return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers() {
-        return Response.ok().build();
+        PanacheQuery<PanacheEntityBase> query = User.findAll();
+
+        return Response.ok(query.list()).build();
     }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id) {
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.delete();
+
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
 }
